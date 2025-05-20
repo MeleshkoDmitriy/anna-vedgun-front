d54a43b6a25bf18676e9235ad6c976a512529d1b
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2025-05-20 20:06:12 +0300

    feat: added deps: tests, husky hooks, env

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // Новые функции
+        'fix', // Исправления ошибок
+        'docs', // Изменения в документации
+        'style', // Изменения, не влияющие на смысл кода
+        'refactor', // Рефакторинг кода
+        'perf', // Изменения для улучшения производительности
+        'test', // Добавление или изменение тестов
+        'chore', // Изменения в процессе сборки или вспомогательных инструментах
+        'revert', // Откат изменений
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'scope-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+  },
+};
---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+}
+
+export default createJestConfig(customJestConfig) 
