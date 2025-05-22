f93ed9b1387507fcdc2595c6c653ee72e1f47f12
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2025-05-22 20:51:46 +0300

    feat: added style variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import styles from './page.module.scss';
 
 export default function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <div className={styles.wrapper}></div>;
 }
