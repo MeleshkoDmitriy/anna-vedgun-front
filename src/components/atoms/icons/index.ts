5360f63f037baba17c4367ea57e8a049cc2e7d0c
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2025-06-03 20:37:30 +0300

    fix: fixed spinner

--- src/components/atoms/icons/index.ts
+++ src/components/atoms/icons/index.ts
@@ -5,7 +5,7 @@ import { GlobeAltIcon as B17Icon } from '@heroicons/react/24/outline';
 import { UserPlusIcon as PlusIcon } from '@heroicons/react/24/outline';
 import { UserMinusIcon as MinusIcon } from '@heroicons/react/24/outline';
 import { ChevronDownIcon as ChevronIcon } from '@heroicons/react/24/outline';
-import { ArrowPathIcon as LoadingIcon } from '@heroicons/react/24/outline';
+import { AiOutlineLoading3Quarters as LoadingIcon } from 'react-icons/ai';
 
 export {
   LocationIcon,
